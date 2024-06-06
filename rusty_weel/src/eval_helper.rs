b1126ecb9f3cc8dde8541a6effe89ca6f64988e4
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2024-06-06 16:38:14 +0200

    Adapt eval_helper to work more ergonomically

--- rusty_weel/src/eval_helper.rs
+++ rusty_weel/src/eval_helper.rs
@@ -1,35 +1,42 @@
-use std::{collections::HashMap, error::Error, fmt::Display};
+use std::{collections::HashMap, fmt::Display};
 
 use log;
 use serde_json::json;
 
+use crate::data_types::{DynamicData, StaticData};
+
 /**
  * Sends a list of expressions and the context to evaluate them in to the evaluation backend
- * If any error occurs then this function panics
  * Returns an error if:
  *  - Request body could not be parsed
- *  - Post request fails
+ *  - Post request fails / Evaluation fails
  */
 // TODO: Currently the context passed to the evaluated is currently only the data, not endpoints/attributes -> Do we also need these?
 // TODO: Yes we need to pass all contained in ManipulateStructure (weel.rb)?
-pub fn evaluate(
+pub fn evaluate_expressions (
     eval_backend: &str,
-    context: String,
-    statements: HashMap<String, String>,
+    dynamic_context: &DynamicData,
+    static_context: &StaticData,
+    expressions: HashMap<String, String>,
 ) -> Result<HashMap<String, String>, EvalError> {
     let client = reqwest::blocking::Client::new();
+
+    // Handle serialization outside of json! macro as it will panic otherwise
+    let static_context = match serde_json::to_string(&static_context) { Ok(x) => x, Err(err) => return Err(EvalError::GeneralEvalError(format!("Could not serialize static context: {}", err))) };
+    let dynamic_context = match serde_json::to_string(&dynamic_context) { Ok(x) => x, Err(err) => return Err(EvalError::GeneralEvalError(format!("Could not serialize static context: {}", err))) };
+    let expressions = match serde_json::to_string(&expressions) { Ok(x) => x, Err(err) => return Err(EvalError::GeneralEvalError(format!("Could not serialize expressions: {}", err))) };
+    
     let body = json!(
     {
-        "context": context,
-        "statements": statements
+        "static_context": static_context,
+        "static_context": dynamic_context,
+        "statements": expressions
     });
     let body = match serde_json::to_string(&body) {
         Ok(x) => x,
         Err(err) => {
             log::error!("Error parsing evaluation request body. Error: {err}");
-            return Err(EvalError {
-                internal_error: Box::new(err),
-            });
+            return Err(EvalError::GeneralEvalError(format!("Could not serialize json objection for serailization request: {}", err)));
         }
     };
 
@@ -47,16 +54,42 @@ pub fn evaluate(
         }
         Err(err) => {
             log::error!("Error evaluating code via eval backend: {eval_backend}, Error: {err}");
-            Err(EvalError {
-                internal_error: Box::new(err),
-            })
+            return Err(EvalError::GeneralEvalError(format!("{}", err)));
+        }
+    }
+}
+
+pub fn evaluate_expression (
+    eval_backend: &str,
+    dynamic_context: &DynamicData,
+    static_context: &StaticData,
+    expression: &str
+) -> Result<String, EvalError> {
+    let mut expressions = HashMap::new();
+    expressions.insert("k".to_owned(), expression.to_owned());
+    let result = evaluate_expressions(eval_backend, dynamic_context, static_context,  expressions);
+    match result {
+        Ok(eval_result) => 
+            match eval_result.get("k") {
+                Some(x) => Ok(x.clone()),
+                None => {
+                    log::error!("failure creating new key value pair. Evaluation failed");
+                    Err(EvalError::GeneralEvalError(format!("Result misses value")))
+                }
+            },
+        Err(err) => {
+            log::error!(
+                "failure creating new key value pair. {}", err
+            );
+            Err(err)
         }
     }
 }
 
 #[derive(Debug)]
-pub struct EvalError {
-    internal_error: Box<dyn Error>,
+pub enum EvalError {
+    GeneralEvalError(String),
+    SyntaxError
 }
 
 impl Display for EvalError {
@@ -64,21 +97,7 @@ impl Display for EvalError {
         write!(
             f,
             "Error occured when evaluating an expression in an external language: {:?}",
-            self.internal_error
+            if matches!(self, EvalError::SyntaxError) {"Syntax error"} else {"general error"}
         )
-    }
-}
-
-impl Error for EvalError {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        Some(self.internal_error.as_ref())
-    }
-
-    fn description(&self) -> &str {
-        "description() is deprecated; use Display"
-    }
-
-    fn cause(&self) -> Option<&dyn Error> {
-        self.source()
     }
 }
