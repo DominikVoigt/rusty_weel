4eba93e64208a530c3a4bd19a6949f5e4eaba364
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2024-07-03 15:11:02 +0200

    remove useless test

--- rusty_weel/src/lib.rs
+++ rusty_weel/src/lib.rs
@@ -7,13 +7,3 @@ pub mod data_types;
 pub mod connection_wrapper;
 pub mod eval_helper;
 pub mod redis_helper;
-
-#[cfg(test)]
-mod test {
-    
-    #[test]
-    fn test_reqwest() {
-        let client = reqwest::Client::new();
-        let request = client.post("http://testing.com");
-    }
-}
