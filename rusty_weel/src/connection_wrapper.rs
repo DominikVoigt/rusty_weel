df0d505b91db900cf7a754182c8124f0fdf4deab
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2024-08-19 13:04:44 +0200

    Extract methods and reformat

--- rusty_weel/src/connection_wrapper.rs
+++ rusty_weel/src/connection_wrapper.rs
@@ -1,14 +1,21 @@
-
 use http_helper::Parameter;
-use reqwest::{header::{HeaderMap, HeaderName, HeaderValue, CONTENT_TYPE}, Method};
+use reqwest::{
+    header::{HeaderMap, HeaderName, HeaderValue, CONTENT_TYPE},
+    Method,
+};
 use serde_json::{json, Value};
 use std::{
-    collections::HashMap, str::FromStr, sync::{Arc, Weak}, thread::sleep, time::{Duration, SystemTime}
+    collections::HashMap,
+    str::FromStr,
+    sync::{Arc, Weak},
+    thread::sleep,
+    time::{Duration, SystemTime},
 };
 
-
-
-use crate::{data_types::{HTTPParams, InstanceMetaData}, dsl_realization::{generate_random_key, Error, Result, Weel}};
+use crate::{
+    data_types::{HTTPParams, InstanceMetaData},
+    dsl_realization::{generate_random_key, Error, Result, Weel},
+};
 
 pub struct ConnectionWrapper {
     weel: Weak<Weel>,
@@ -52,6 +59,7 @@ impl ConnectionWrapper {
     pub fn loop_guard(&self, id: String) {
         let attributes = &self.weel().static_data.attributes;
         let loop_guard_attribute = attributes.get("nednoamol");
+        // TODO: Return if loop guard true?
         if loop_guard_attribute.is_some_and(|attrib| attrib == "true") {
             return;
         }
@@ -92,8 +100,7 @@ impl ConnectionWrapper {
         content.insert("state".to_owned(), new_state.to_owned());
 
         let weel = self.weel();
-        weel
-            .redis_notifications_client
+        weel.redis_notifications_client
             .lock()
             .expect("Failed to lock mutex")
             .notify(
@@ -109,8 +116,7 @@ impl ConnectionWrapper {
         content.insert("message".to_owned(), err.as_str().to_owned());
 
         let weel = self.weel();
-        weel
-            .redis_notifications_client
+        weel.redis_notifications_client
             .lock()
             .expect("Could not acquire mutex")
             .notify(
@@ -126,8 +132,7 @@ impl ConnectionWrapper {
         content.insert("message".to_owned(), err.as_str().to_owned());
 
         let weel = self.weel();
-        weel
-            .redis_notifications_client
+        weel.redis_notifications_client
             .lock()
             .expect("Could not acquire mutex")
             .notify(
@@ -139,8 +144,7 @@ impl ConnectionWrapper {
 
     pub fn inform_position_change(&self, ipc: Option<HashMap<String, String>>) {
         let weel = self.weel();
-        weel
-            .redis_notifications_client
+        weel.redis_notifications_client
             .lock()
             .expect("Could not acquire mutex")
             .notify(
@@ -157,25 +161,43 @@ impl ConnectionWrapper {
     pub fn prepare(&mut self, endpoints: &Vec<String>) {
         if endpoints.len() > 0 {
             let weel = self.weel();
-            self.handler_endpoints = endpoints.iter().map(|ep| {
-                weel
-                    .dynamic_data
-                    .endpoints
-                    .get(ep)            
-            })
-            .filter(|item| item.is_some())
-            .map(|item| item.expect("safe to unwrap").clone())
-            .collect();
+            self.resolve_endpoints(endpoints, &weel);
 
-            if weel.static_data.attributes.get("twin_engine").map(|attr| !attr.is_empty()).unwrap_or(false) {
-                self.handler_endpoint_origin = self.handler_endpoints.clone();
-                let twin_engine: &str = &weel.static_data.attributes.get("twin_engine").expect("Cannot happen");
+            match weel.static_data.attributes.get("twin_engine") {
+                Some(flag) => {
+                    if !flag.is_empty() {
+                        self.handler_endpoint_origin = self.handler_endpoints.clone();
+                        let twin_engine: &str = &weel
+                            .static_data
+                            .attributes
+                            .get("twin_engine")
+                            .expect("Cannot happen");
 
-                // TODO: Replace the endpoints part: `Riddl::Protocols::Utils::escape`
-                let endpoints = self.handler_endpoints.get(0).expect("");
-                self.handler_endpoints = vec![format!("{}?original_endpoint={}", twin_engine, endpoints)];
+                        // TODO: Replace the endpoints part: `Riddl::Protocols::Utils::escape`
+                        let endpoints = self.handler_endpoints.get(0).expect("");
+                        self.handler_endpoints =
+                            vec![format!("{}?original_endpoint={}", twin_engine, endpoints)];
+                    }
+                }
+                None => {
+                    // Do nothing
+                },
             }
         }
+    }
+
+    /**
+     * Resolves the endpoint names in endpoints to the actual endpoint URLs
+     */
+    fn resolve_endpoints(&mut self, endpoints: &Vec<String>, weel: &Arc<Weel>) {
+        self.handler_endpoints = endpoints
+            .iter()
+            .map(|ep| weel.dynamic_data.endpoints.get(ep))
+            .filter_map(|item| match item {
+                Some(item) => Some(item.clone()),
+                None => None,
+            })
+            .collect();
     }
 
     pub fn additional(&self) -> Value {
@@ -204,36 +226,53 @@ impl ConnectionWrapper {
      *      - Only the standard headers that are generated in the `henerate_headers` method are send.
      *      - All arguments wihtin the HTTPParams are send as Key-Value pairs as part of the body (application/x-www-form-urlencoded)
      */
-    pub fn curl(self: &Arc<Self>, parameters: &HTTPParams) -> Result<()>{
+    pub fn curl(self: &Arc<Self>, parameters: &HTTPParams) -> Result<()> {
         let weel = self.weel();
         let callback_id = generate_random_key();
-        let headers: HeaderMap = self.generate_headers(weel.static_data.get_instance_meta_data(), &callback_id)?;
+        let headers: HeaderMap =
+            self.generate_headers(weel.static_data.get_instance_meta_data(), &callback_id)?;
         let mut params = Vec::new();
         // Put arguemnts into SimpleParameters that will be part of the body-> Since we cannot upload files from the CPEE for now // TODO?
         match parameters.arguments.as_ref() {
             Some(args) => args.iter().for_each(|arg| {
-                    let value = arg.value.clone().unwrap_or("".to_owned());
-                    params.push(http_helper::Parameter::SimpleParameter { name: arg.key.to_owned(), value, param_type: http_helper::ParameterType::Body });
-                }),
-            None => {log::info!("Arguments provided to protocurl are empty");}
+                let value = arg.value.clone().unwrap_or("".to_owned());
+                params.push(http_helper::Parameter::SimpleParameter {
+                    name: arg.key.to_owned(),
+                    value,
+                    param_type: http_helper::ParameterType::Body,
+                });
+            }),
+            None => {
+                log::info!("Arguments provided to protocurl are empty");
+            }
         };
 
         let mut status: u16;
         let mut response_headers: HeaderMap;
         loop {
             let mut content_json = HashMap::new();
-            content_json.insert("activity_uuid".to_owned(), self.handler_activity_uuid.clone());
+            content_json.insert(
+                "activity_uuid".to_owned(),
+                self.handler_activity_uuid.clone(),
+            );
             content_json.insert("label".to_owned(), self.label.clone());
-            let position = self.position.as_ref().map(|x| x.clone()).unwrap_or("".to_owned());
+            let position = self
+                .position
+                .as_ref()
+                .map(|x| x.clone())
+                .unwrap_or("".to_owned());
             content_json.insert("activity".to_owned(), position);
-            
+
             // TODO: Handler Passthrough? -> When task is called that was async and instance was stopped in between
             weel.callback(Arc::clone(self), &callback_id, content_json);
-            
+
             // TODO: Determine wheter we need to subsitute the url like in connection.rb
-            let mut client = http_helper::Client::new(self.handler_endpoints.get(0).expect("No endpoint provided"), Method::GET)?;
+            let mut client = http_helper::Client::new(
+                self.handler_endpoints.get(0).expect("No endpoint provided"),
+                Method::GET,
+            )?;
             client.set_request_headers(headers.clone());
-            
+
             /*
             // Run request
             let response = client.execute()?;
@@ -263,14 +302,14 @@ impl ConnectionWrapper {
 
                 if callback_header_is_set {
                     if response_body.is_empty() {
-                        
+
                     } else {
                         response_headers.insert("CPEE_UPDATE".to_owned(), HeaderValue::from_str("true").expect("This cannot fail"));
-                        
+
                         self.callback(vec![Parameter::ComplexParamter { name: "response".to_owned(), mime_type: content_type.to_owned(), content_handle: response_file }], response_headers)
                     }
                 }
-                 
+
             }
              */
         }
@@ -278,29 +317,42 @@ impl ConnectionWrapper {
         todo!()
     }
 
-    pub fn callback(&self, parameters: Vec<Parameter>, headers: HashMap<String, String>) {
-
-    }
+    pub fn callback(&self, parameters: Vec<Parameter>, headers: HashMap<String, String>) {}
 
     fn generate_headers(&self, data: InstanceMetaData, callback_id: &str) -> Result<HeaderMap> {
         let position = self.position.as_ref().map(|x| x.as_str()).unwrap_or("");
         let twin_target = data.attributes.get("twin_target");
         let mut headers = HeaderMap::new();
-        headers.append("CPEE-BASE",            HeaderValue::from_str(&data.cpee_base_url)?);
-        headers.append("CPEE-Instance",        HeaderValue::from_str(&data.instance_id)?);
-        headers.append("CPEE-Instance-URL",    HeaderValue::from_str(&data.instance_url)?);
-        headers.append("CPEE-Instance-UUID",   HeaderValue::from_str(&data.instance_uuid)?);
-        headers.append("CPEE-CALLBACK",        HeaderValue::from_str(&format!("{}/callbacks/{}/", &data.instance_url, callback_id))?);
-        headers.append("CPEE-CALLBACK-ID",     HeaderValue::from_str(callback_id)?);
-        headers.append("CPEE-ACTIVITY",        HeaderValue::from_str(&position)?);
-        headers.append("CPEE-LABEL",           HeaderValue::from_str(&self.label)?);
+        headers.append("CPEE-BASE", HeaderValue::from_str(&data.cpee_base_url)?);
+        headers.append("CPEE-Instance", HeaderValue::from_str(&data.instance_id)?);
+        headers.append(
+            "CPEE-Instance-URL",
+            HeaderValue::from_str(&data.instance_url)?,
+        );
+        headers.append(
+            "CPEE-Instance-UUID",
+            HeaderValue::from_str(&data.instance_uuid)?,
+        );
+        headers.append(
+            "CPEE-CALLBACK",
+            HeaderValue::from_str(&format!(
+                "{}/callbacks/{}/",
+                &data.instance_url, callback_id
+            ))?,
+        );
+        headers.append("CPEE-CALLBACK-ID", HeaderValue::from_str(callback_id)?);
+        headers.append("CPEE-ACTIVITY", HeaderValue::from_str(&position)?);
+        headers.append("CPEE-LABEL", HeaderValue::from_str(&self.label)?);
         if let Some(twin_target) = twin_target {
             headers.append("CPEE-TWIN-TARGET", HeaderValue::from_str(twin_target)?);
         }
 
         for attribute in data.attributes.iter() {
             let key: String = format!("CPEE-ATTR-{}", attribute.0.replace("_", "-"));
-            headers.append(HeaderName::from_str(&key)?, HeaderValue::from_str(attribute.1)?);
+            headers.append(
+                HeaderName::from_str(&key)?,
+                HeaderValue::from_str(attribute.1)?,
+            );
         }
         Ok(headers)
     }
@@ -312,12 +364,12 @@ impl ConnectionWrapper {
         match self.weel.upgrade() {
             Some(weel) => weel,
             None => {
-                log::error!("Weel instance no longer exists, this instance should have been dropped...");
+                log::error!(
+                    "Weel instance no longer exists, this instance should have been dropped..."
+                );
                 // Todo: What should we do here?
                 panic!()
-            },
+            }
         }
     }
 }
-
-
