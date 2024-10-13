5e590cf8d3c7348e3f5fb865cf485b62bda3f2d4
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2024-10-13 20:30:42 +0200

    debug

--- rusty_weel/src/redis_helper.rs
+++ rusty_weel/src/redis_helper.rs
@@ -121,6 +121,7 @@ impl RedisHelper {
             instance_id,
             serde_json::to_string_pretty(&payload).expect("Could not deserialize payload")
         );
+        println!("Payload: {payload}");
         log::debug!("Publishing on channel {:?} the message: {:?}", channel, payload);
         let publish_result: RedisResult<()> = self.connection.publish(channel, payload);
         match publish_result {
