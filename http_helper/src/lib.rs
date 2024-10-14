6161fe5271428ee1198071d45b689ba61b2ec1e3
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2024-10-14 17:23:27 +0200

    Add debug

--- http_helper/src/lib.rs
+++ http_helper/src/lib.rs
@@ -211,6 +211,7 @@ impl Client {
      * (128-255).
      */
     pub fn add_request_header(&mut self, name: &str, value: &str) -> Result<()> {
+        log::debug!("Setting request header: {}={}", name, value);
         self.headers.remove(name);
         self.headers
             .insert(HeaderName::from_str(name)?, HeaderValue::from_str(value)?);
