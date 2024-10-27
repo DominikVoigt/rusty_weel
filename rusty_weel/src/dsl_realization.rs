c1c7bd7469c532ca99ecca85c77d23de7f811338
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2024-10-27 17:49:37 +0100

    adapt model

--- rusty_weel/src/dsl_realization.rs
+++ rusty_weel/src/dsl_realization.rs
@@ -2077,7 +2077,7 @@ mod test {
             }
         };
         let stat = StaticData {
-            instance_id: 228,
+            instance_id: 229,
             host: "localhost".to_owned(),
             cpee_base_url: "https://echo.bpm.in.tum.de/flow/engine".to_owned(),
             redis_url: None,
