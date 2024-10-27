3c9ade07e6ae60367a008f6dbea01595da7e874a
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2024-10-27 17:39:53 +0100

    adapt model

--- rusty_weel/src/main.rs
+++ rusty_weel/src/main.rs
@@ -32,7 +32,7 @@ fn main() {
     let (stop_signal_sender, stop_signal_receiver) = mpsc::channel::<()>();
     *WEEL.stop_signal_receiver.lock().unwrap() = Some(stop_signal_receiver);
     let model = || -> Result<()> {
-        inject!("./resources/parallel_last.eic");
+        inject!("./resources/parallel_last_simple.eic");
         Ok(())
     };
 
