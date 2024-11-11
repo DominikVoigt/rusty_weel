f9cf9404ffe987b0bcd3f3a565147effd59e359a
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2024-11-11 15:28:45 +0100

    add dbg

--- weel_lib/src/connection_wrapper.rs
+++ weel_lib/src/connection_wrapper.rs
@@ -616,7 +616,7 @@ impl ConnectionWrapper {
             response_headers = header_map_to_hash_map(&response.headers)?;
             body = response.body;
 
-            log::debug!("Received response headers: {:?}", response_headers);
+            log::debug!("Received response headers: {:?}, status: {status}", response_headers);
 
             if status == 561 {
                 match weel.opts.attributes.get("sim_translate") {
