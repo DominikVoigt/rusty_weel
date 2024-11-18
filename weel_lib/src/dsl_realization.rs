965bb3919e4d40e81eeb0a9701b361a16da0f424
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2024-11-18 14:40:56 +0100

    dbg

--- weel_lib/src/dsl_realization.rs
+++ weel_lib/src/dsl_realization.rs
@@ -1648,6 +1648,7 @@ impl Weel {
         call_result: Option<Value>,
         call_headers: Option<HashMap<String, String>>,
     ) -> Result<eval_helper::EvaluationResult> {
+        log::debug!("Using call result: {:?}", call_result);
         // We clone the dynamic data and status dto here which is expensive but allows us to not block the whole weel until the eval call returns
         if read_only {
             let dynamic_data = self.context.lock().unwrap().clone();
