6bdbfc07c490e55d8cdc1a74b4ba6ff526541ebf
Dominik Voigt <dominik.ingo.voigt@gmail.com>
2025-05-26 18:45:50 +0200

    add heaptrack script

---
+++ run_heaptrack.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+cd ./target/release
+# Add both paths depending on whether cross compile toolchain is installed or not
+export LD_LIBRARY_PATH+=:./:$(rustc --print=sysroot)/lib/rustlib/x86_64-unknown-linux-gnu/lib:$(rustc --print=sysroot)/lib
+heaptrack ./weel_bin
